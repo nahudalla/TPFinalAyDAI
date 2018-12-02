142642b532c2d9d836d18ac4f2b036f7cdd19eaf
Nahuel Dallacamina <nahudalla@gmail.com>
2018-12-01 21:19:08 -0300

    Comparator class implemented

---
+++ src/native/cpp/include/algor/Comparator.hpp
@@ -0,0 +1,17 @@
+#ifndef TPFINALAYDAI_COMPARATOR_HPP
+#define TPFINALAYDAI_COMPARATOR_HPP
+
+namespace algor {
+    // TODO: tests
+
+
+    template <typename T>
+    class Comparator {
+    public:
+        virtual int compare(T const& lhs, T const& rhs) const {
+            return (lhs < rhs) ? -1 : ((rhs < lhs) ? 1 : 0);
+        }
+    };
+}
+
+#endif //TPFINALAYDAI_COMPARATOR_HPP
